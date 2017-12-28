e5183ac67d32936ce98669386dc25a831679368b
luisa.scavo <luisa.scavo@nyc-055297-mbp.local>
2017-12-28 09:44:03 -0500

    add @tags, format_tags_list helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,18 @@ class ApplicationController < Sinatra::Base
   get '/' do
     @title = "We'd like to help."
     @resources = Resource.approved
+    @tags = Tag.all
     erb :index
+  end
+
+  helpers do
+    def format_tag_list
+      tags = []
+      @tags.each do |t|
+        tags << "<a href='/tags/#{t.slug}'>#{t.name}</a>"
+      end
+      tags.join(" | ")
+    end
   end
 
 end
