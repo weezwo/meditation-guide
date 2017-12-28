fdbeb0ffab9f48145e6df7ae6186ddc0f02bb7bd
luisa.scavo <luisa.scavo@nyc-055297-mbp.local>
2017-12-28 09:42:30 -0500

    create slug methods and associations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,11 @@
 class Tag < ActiveRecord::Base
+  has_many :resource_tags
+  has_many :resources, through: :resource_tags
+  def slug
+    self.name.downcase.gsub(" ","-")
+  end
+
+  def self.unslug(slug)
+    self.find_by(name: slug.capitalize.gsub("-"," "))
+  end
 end
